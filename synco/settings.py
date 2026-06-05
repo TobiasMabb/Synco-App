29bea544e6ccbaa641bcaacc394c819232ad0c2e
Vince Alshie Rivera <202280400@psu.palawan.edu.ph>
2026-06-05 21:42:52 +0800

    Add superuser

--- synco/settings.py
+++ synco/settings.py
@@ -210,3 +210,16 @@ LOGGING = {
         },
     },
 }
+
+
+if os.environ.get("CREATE_SUPERUSER") == "true":
+    from django.contrib.auth import get_user_model
+
+    User = get_user_model()
+
+    if not User.objects.filter(username="admin").exists():
+        User.objects.create_superuser(
+            username="admin",
+            email="admin@synco.com",
+            password="admin12345"
+        )
